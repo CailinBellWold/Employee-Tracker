0270e1852841b094384ec3c0266c2ef3a42c7e13
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-06-20 22:09:23 -0700

    Updating seed data.

--- seeds/seed.sql
+++ seeds/seed.sql
@@ -14,16 +14,16 @@ VALUES (3, 'Development');
 ----- ROLE SEEDS -----
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Executive Director", 200000, 3);
+VALUES (1, 'Executive Director', 200000, 3);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (2, "Director of Operations", 150000, 1);
+VALUES (2, 'Director of Operations', 150000, 1);
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (3, 'Director of Finance & Administration', 150000, 2);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (4, "Director of Development", 150000, 3);
+VALUES (4, 'Director of Development', 150000, 3);
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (5, 'Programming Manager', 100000, 1);
@@ -49,31 +49,31 @@ VALUES (11, 'Corporate Gifts Manager', 100000, 3);
 ----- EMPLOYEE SEEDS -----
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Imani", "Williams", 1, null);
+VALUES (1, 'Imani', 'Williams', 1, null);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Darius", "Hernandez", 2, 1);
+VALUES (2, 'Darius', 'Hernandez', 2, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (3, "Amy", "Choi", 3, 1);
+VALUES (3, 'Amy', 'Choi', 3, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (4, "Dara", "Sok", 4, 1);
+VALUES (4, 'Dara', 'Sok', 4, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (5, "Melissa", "Brown", 5, 2);
+VALUES (5, 'Melissa', 'Brown', 5, 2);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (6, "Amira", "Ali", 6, 3);
+VALUES (6, 'Amira', 'Ali', 6, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (7, "Alexus", "Cole", 7, 3);
+VALUES (7, 'Alexus', 'Cole', 7, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (8, "Lianne", "Soon", 8, 4);
+VALUES (8, 'Lianne', 'Soon', 8, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (9, "Jamal", "Jones", 9, 4);
+VALUES (9, 'Jamal', 'Jones', 9, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (10, "Pedro", "Rodriguez",10, 4);
+VALUES (10, 'Pedro', 'Rodriguez',10, 4);
